8ccaa3401952b8079400e441f1d44e9b08712ec1
Varringard <115581828+Varringard@users.noreply.github.com>
2025-04-13 15:58:49 +0300

    Delete src directory

--- src/globals.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "libmem/libmem.h"
-
-#include <cstdio>
-
-extern lm_module_t g_modSteamClient;
-
-extern uint32_t g_currentUserAccountId;
--- src/sdk/IClientUser.cpp
+++
@@ -1,8 +0,0 @@
-#include "IClientUser.hpp"
-
-#include "../utils.hpp"
-#include "../vftableinfo.hpp"
-
-#include <cstdint>
-
-IClientUser* g_pSteamUser;
